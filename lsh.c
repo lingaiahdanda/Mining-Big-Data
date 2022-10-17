9422011d3ed78b6da781004de59977a6e879f915
lingaiahdanda <lingaiahdanda@gmail.com>
2022-10-16 19:33:09 -0500

    Added finding closest point in the cluster

--- lsh.c
+++ lsh.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<math.h>
+#include<float.h>
 int nclusters =0;
 int *cluster_ass;
 int LSH(int dim, int ndata, double *data,int m, double W, double **h, double *b,int *cluster_start, int *cluster_size, int **cluster_hashval);
 int search_LSH(int dim, int ndata, double *data,int m, double W, double **h, double *b,int nclusters, int *cluster_start, int *cluster_size, int **cluster_hashval,double *query_pt, double *result_pt) ;
+void closestPoint(int cluster,int ndata,double *query_pt,int dim,double *data);
 
 int LSH(int dim, int ndata, double *data,int m, double W, double **h, double *b,int *cluster_start, int *cluster_size, int **cluster_hashval){
     int *cluster_assign = malloc((ndata)*sizeof(int));
@@ -58,6 +60,7 @@ int LSH(int dim, int ndata, double *data,int m, double W, double **h, double *b,
     cluster_ass = cluster_assign;
     return nclusters;
 }
+//searching the points
 int search_LSH(int dim, int ndata, double *data,int m, double W, double **h, double *b,int nclusters, int *cluster_start, int *cluster_size, int **cluster_hashval,double *query_pt, double *result_pt){
     //calculating hashvalue of query point
     int resHashVal[m];
@@ -86,25 +89,59 @@ int search_LSH(int dim, int ndata, double *data,int m, double W, double **h, dou
     }
     //the closest points in the cluster are
     if(isClusterFound){
+        closestPoint(cluster,ndata,query_pt,dim,data);
+    }else{
+        double minSum = DBL_MAX;
+        int closest_cluster;
+        for(int i=0;i<nclusters;i++){
+            double sum = (double)0;
+            for(int j=0;j<m;j++){
+                sum = sum + pow((resHashVal[j]-cluster_hashval[i][j]),2);
+            }
+            if(sum < minSum){
+                minSum = sum;
+                closest_cluster = i;
+            }
+        }
+        printf("\nNo cluster found\nThe closest cluster is %d\n",closest_cluster);
+        closestPoint(closest_cluster,ndata,query_pt,dim,data);
+    }
+}
+
+//find closest point
+void closestPoint(int cluster,int ndata,double *query_pt,int dim,double *data){
+    double minSum = DBL_MAX;
+        int closest_point;
         printf("The query point belongs to the cluster %d ", cluster);
         printf("\nThe points in the cluster are:-\n");
         for(int i=0;i<ndata;i++){
             if(cluster_ass[i] == cluster){
+                double sum=(double)0;
+                int iterator = 0;
                 for(int j=i;j<i+dim;j++){
+                    sum = sum + (pow((query_pt[iterator]-data[j]),2));
+                    iterator = iterator+1;
                     printf("%f ",data[j]);
+                }
+                printf(" Distance from query pt-> %f ",sqrt(sum));
+                if(sqrt(sum) < minSum){
+                    minSum = sum;
+                    closest_point = i;
                 }
                 printf("\n");
             }
         }
-    }else{
-        printf("\nNo cluster found");
-    }
+        printf("The closest point is %d and the point is :-\n",closest_point);
+        for(int j=closest_point;j<closest_point+dim;j++){
+            printf("%f ",data[j]);
+        }
+        printf("\n");
 }
 int main(){
-    int ndata = 100;
+    int ndata = 1000000;
     int dim = 16;
     int m = 5;
-    double W = 0.5;
+    double W = 0.3;
     double *b  = malloc((m)*sizeof(double));
     double *data = malloc((ndata*dim)*sizeof(double));
     double **h = (double**)malloc(m*sizeof(double*));
